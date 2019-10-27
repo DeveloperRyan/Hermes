e5c0f09322188ec063a05950de04f2351953b8d7
Ryan Elliott <rselliott99@gmail.com>
2019-10-27 02:58:00 -0400

    Add listFaces function. Code cleanup"

--- main.py
+++ main.py
@@ -20,7 +20,7 @@
 
 # MVP
 # --------------
-# Image taken > uploaded to S31
+# Image taken > uploaded to S3
 # Face ID found
 # Look up face ID in database
 # Fetch transaction information
@@ -31,6 +31,7 @@
 
 
 import boto3
+from datetime import datetime
 
 client = boto3.client('rekognition') # Connect to rekognition client
 s3 = boto3.client('s3') # Connect to s3 client
@@ -38,20 +39,20 @@ s3 = boto3.client('s3') # Connect to s3 client
 # AWS information
 bucket = 'image-rewards'
 collection = 'registered_faces'
-fileInput = ''
+image_input = ''
 
 # Create a collection within bucket to store and search images
-def create_collection(collection):
+def createCollection(collection):
     client.create_collection(CollectionId=collection)
     print('Collection created')
 
 # Delete a collection
-def delete_collection(collection):
+def deleteCollection(collection):
     client.delete_collection(CollectionId=collection)
     print('Collection deleted')
 
 # List all created collections
-def list_collections(max_results):
+def listCollections(max_results):
     collection_response = client.list_collections(MaxResults=max_results)
     collection_list = []
 
@@ -71,33 +72,43 @@ def list_collections(max_results):
     return collection_list
 
 # Get number of faces in a collection
-def collection_facecount(collection):
+def collectionFacecount(collection):
     facecount_response = client.describe_collection(CollectionId=collection)
     return facecount_response['FaceCount']
 
 # Index a face to a collection
-def index_face(image, collection):
-    indexResponse = client.index_faces(CollectionId=collection,
+def indexFace(image, collection):
+    index_response = client.index_faces(CollectionId=collection,
                     Image={'S3Object':{'Bucket':bucket, 'Name':image}},
                     MaxFaces=1,
                     QualityFilter='NONE')
-    return indexResponse['FaceRecords'][0]['Face']['FaceId']
+    return index_response['FaceRecords'][0]['Face']['FaceId']
 
 # Remove a list of faces from a collection
-def delete_faces(face_ids, collection): # Pass a vector of faceIDs
+def deleteFaces(face_ids, collection): # Pass a vector of faceIDs
     client.delete_faces(CollectionId=collection,
                         FaceIds=face_ids)
 
 # Remove a single face from a collection
-def delete_face(face_id, collection):
+def deleteFace(face_id, collection):
     face_id_list = [face_id]
     client.delete_faces(CollectionId=collection,
                         FaceIds=face_id_list )
 
 # Searches for a specific face that matches a given image
-def search_for_face(input, collection):
-    searchResponse = client.search_faces_by_image(CollectionId=collection,
+def searchForFace(input, collection):
+    search_response = client.search_faces_by_image(CollectionId=collection,
         Image={'S3Object':{'Bucket':bucket, 'Name':input}},
         FaceMatchThreshold=80,
         MaxFaces=1)
-    return searchResponse['FaceMatches'][0]['Face']['FaceId']
+    return search_response['FaceMatches'][0]['Face']['FaceId']
+
+def listFaces(collection):
+    faces = []
+    list_response = client.list_faces(CollectionId=collection)
+    for face in list_response['Faces']:
+        faces.append(face['FaceId'])
+    return faces
+
+
+print(listFaces(collection))
